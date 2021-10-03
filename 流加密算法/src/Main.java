c7e0d4160574b9cb1a65ca76054ebe117c0681ee
aq949342769 <949342769@qq.com>
2021-10-03 16:55:34 +0800

    加入了UI

--- 流加密算法/src/Main.java
+++ 流加密算法/src/Main.java
@@ -1,16 +1,83 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        File file = new File("src");
+        String curPath = file.getAbsolutePath();
+        Frame frame = new Frame();
+        FileDialog f1 = new FileDialog(frame, "选择要加密的文件", FileDialog.LOAD);
+        FileDialog f2 = new FileDialog(frame, "选择要解密的文件", FileDialog.LOAD);
+        FileDialog f3 = new FileDialog(frame, "选择密钥文件", FileDialog.LOAD);
+
+        f1.setDirectory(curPath);
+        f2.setDirectory(curPath);
+        f3.setDirectory(curPath);
+
+        Button b1 = new Button("加密文件");
+        Button b2 = new Button("解密文件");
+        Button b3 = new Button("选择密钥");
+
         // 获取明文数据
-        File msgFile = new File("src/msg.txt");
-        File keyFile = new File("src/key.txt");
+        final File[] msgFile = {null};
+        final File[] keyFile = {new File("src/key.txt")};//默认选择工程内的密钥
+
         // 加密
-        encrypt(msgFile, keyFile);
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                f1.setVisible(true);
+                msgFile[0] = new File(f1.getDirectory() + f1.getFile());
+                try {
+                    encrypt(msgFile[0], keyFile[0]);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+         }
+        );
         // 解密
-        decrypt(msgFile, keyFile);
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                f2.setVisible(true);
+                try {
+                    decrypt(new File(f2.getDirectory() + f2.getFile()), keyFile[0]);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        // 获取密钥
+        b3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                f3.setVisible(true);
+                keyFile[0] = new File(f3.getDirectory() + f3.getFile());
+
+            }
+        });
+
+        frame.add(b1, BorderLayout.NORTH);
+        frame.add(b2, BorderLayout.CENTER);
+        frame.add(b3, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setVisible(true);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
     }
 
     // 加密过程
@@ -18,7 +85,7 @@ public class Main {
         // 获取密钥
         FileInputStream fis = new FileInputStream(msgFile);
         String fileName = msgFile.getName();
-        File cipherText = new File("src","CText_"+fileName);
+        File cipherText = new File("src", "CText_" + fileName);
         FileOutputStream fos = new FileOutputStream(cipherText);
         int by, count = 0;
         List<Integer> key = getKey(keyFile);
@@ -31,12 +98,12 @@ public class Main {
     }
 
     // 解密函数
-    public static void decrypt(File msgFile, File keyFile) throws IOException {
-        String fileName = msgFile.getName();
-        File cypherFile = new File("src","CText_"+fileName);
-        if (cypherFile.exists()){
+    public static void decrypt(File cypherFile, File keyFile) throws IOException {
+        if (cypherFile.getName().startsWith("CText_")) {
+            String fileName = cypherFile.getName();
+            String[] cText_s = fileName.split("_");
             FileInputStream fis = new FileInputStream(cypherFile);
-            FileOutputStream fos = new FileOutputStream("src/MText_"+fileName);
+            FileOutputStream fos = new FileOutputStream("src/MText_"+ cText_s[1]);
             List<Integer> key = getKey(keyFile);
             int by, count = 0;
             while ((by = fis.read()) != -1) {
@@ -44,11 +111,12 @@ public class Main {
             }
             fis.close();
             fos.close();
-        }else {
-            System.out.println("没有此文件");
+        } else {
+            System.out.println("该文件不是加密文件");
         }
 
     }
+
     // 获取密钥序列
     public static List<Integer> getKey(File key) throws IOException {
         List<Integer> res = new ArrayList<>();

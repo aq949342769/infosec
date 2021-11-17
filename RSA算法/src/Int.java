6474aed481fa3c3e30f081bdbadac83e2461006a
aq949342769 <949342769@qq.com>
2021-11-18 01:18:51 +0800

    RSA模块

---
+++ RSA算法/src/Int.java
@@ -0,0 +1,18 @@
+public class Int {
+    private int x;
+
+    public void set(int x) {
+        this.x = x;
+    }
+
+    public Int(int x) {
+        this.x = x;
+    }
+
+    public Int() {
+    }
+
+    public int get() {
+        return x;
+    }
+}
--- RSA算法/src/Main.java
+++ RSA算法/src/Main.java
@@ -1,3 +1,7 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.util.ArrayList;
+
 public class Main {
     static int gcd(int a, int b) {
         if (a == 0 || b == 0) return 0;
@@ -17,27 +21,106 @@ public class Main {
         return true;
     }
 
-    static int[] exgcd(int a, int b, int x, int y) {
-        if (a < b) return exgcd(b, a, x, y);
-        else if (b == 0) {
-            x = 1;
-            y = 0;
-            return new int[]{a, x, y};
+    // 递归法
+    // static int exgcd(int a, int b, Int x, Int y) {
+    //     if (a < b) return exgcd(b, a, y, x);
+    //     else if (b == 0) {
+    //         x.set(1);
+    //         y.set(0);
+    //         return a;
+    //     } else {
+    //         Int x1 = new Int();
+    //         int d = exgcd(b, b % a, x1, x);
+    //         y.set(x1.get() - a / b * x.get());
+    //         return d;
+    //     }
+    //
+    // }
+    // 迭代法
+    static int exgcd(int a, int b, Int x, Int y) {
+        if (a < b) return exgcd(b, a, y, x);
+        int m = 0, n = 1;
+        x.set(1);
+        y.set(0);
+        while (b != 0) {
+            int d = a / b, t;
+            t = m;
+            m = x.get() - d * t;
+            x.set(t);
+            t = n;
+            n = y.get() - d * t;
+            y.set(t);
+            t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+
+    // 计算私钥（乘法逆元）返回-1 表示逆元不存在
+    static int InvMod(int fin, int e) {
+        Int x = new Int();
+        Int y = new Int();
+        if (exgcd(fin, e, x, y) == 1) {
+            x.set(x.get() % e);
+            return y.get() >= 0 ? y.get() : fin + y.get();
         } else {
-
-
+            return -1;
         }
     }
 
-    // 计算私钥（乘法逆元）
-    static int InvMod(int a, int m) {
 
+    /**
+     * @param a 明文/密文
+     * @param n 公钥/私钥
+     * @param m 模长
+     * @return 密文/明文
+     */
+    static int ExpMod(int a, int n, int m) {
+        ArrayList<Integer> byarr = new ArrayList<>();
+        // 计算指数n的二进制
+        while (n != 0) {
+            byarr.add(n % 2);
+            n /= 2;
+        }
+        // 计算a的中间结果
+        ArrayList<Integer> tmp = new ArrayList<>();
+        int byarrLen = byarr.size();
+        int preNum = 0;
+        for (int i = 0; i < byarrLen; i++) {
+            if (i == 0) {
+                tmp.add(a % m);
+                preNum = a % m;
+            } else {
+                tmp.add((preNum * preNum) % m);
+                preNum = preNum * preNum % m;
+            }
+        }
+        // 根据二进制和a的中间结果计算最终结果
+        int res = 1;
+        for (int i = byarrLen - 1; i >= 0; i--) {
+            if (byarr.get(i) != 0) {
+                res = (res * tmp.get(i)) % m;
+            }
+
+        }
+        return res;
     }
 
-    // static int ExpMod(int a, int n, int m) {
-    // }
-
     public static void main(String[] args) {
-        System.out.println(isPrime(11111));
+        // 选取质数
+        int p = 19, q = 23;
+        int fin = (p - 1) * (q - 1);
+        int n = p * q;
+        // 公钥
+        int e = 17;
+        // 加密
+        int c = ExpMod(47, 17, 437);
+        System.out.println("ctext:" + c);
+        // 计算密钥
+        int d = InvMod(fin, e);
+        System.out.println("mtext:" + ExpMod(c, d, n));
+
+
     }
 }
